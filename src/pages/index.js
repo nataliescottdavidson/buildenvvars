a65e0080e7ea2e212ad64e96b2d101f9cd7dc1ea
Nat <davidson@cloudflare.com>
2020-11-18 12:42:45 -0500

    add smiley face

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Layout from '../components/layout'
 const IndexPage = () => (
   <Layout>
     <h2>
+      🤓
       This site shows multiple ways of using ENV variables as shown in the{' '}
       <a href="https://www.gatsbyjs.org/docs/environment-variables">
         documentation
